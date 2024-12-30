245d5bb7e2fcb3ee8b0bb6f3f0cab15f3877d0cd
Pustrik <dmitrypustovalov2000@gmail.com>
2024-12-30 14:31:46 +0100

    notes upd

--- src/customers/customers.ts
+++ src/customers/customers.ts
@@ -14,7 +14,7 @@ export default function (config: ClientTypes.MatadorClientConfig): CustomersReso
                 {
                     method: "POST",
                     host: hosts.engagementHost,
-                    path: "/create",
+                    path: "/notes",
                 }, 
                 {
                     text, 
